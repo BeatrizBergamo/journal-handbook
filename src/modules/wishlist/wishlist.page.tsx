ec1ce2cce76c0d5fbba6ac519e7235a93ffbcde5
LucasGobatto <lucas.bisaio@live.com>
2023-05-01 18:08:18 -0300

    feat: wishlist page

---
+++ src/modules/wishlist/wishlist.page.tsx
@@ -0,0 +1,10 @@
+import { strings } from "@components/constants";
+import { H1 } from "@components/typography";
+
+export const WishlistPage = () => {
+  return (
+    <>
+      <H1>{strings.wishlist.title}</H1>
+    </>
+  );
+};
