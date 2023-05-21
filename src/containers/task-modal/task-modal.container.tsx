580fddf792a8d6d4e34a8ceeffc7ae75e33d4601
LucasGobatto <lucas.bisaio@live.com>
2023-05-21 16:34:01 -0300

    feat: tag component

--- src/containers/task-modal/task-modal.container.tsx
+++ src/containers/task-modal/task-modal.container.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-import { strings } from "@components/constants";
+import { Divider, Separator } from "@components/box";
+import { Spacing, strings } from "@components/constants";
+import { Flex } from "@components/flex";
 import { Modal } from "@components/modal";
+import { Tag } from "@components/tag/tag.component";
 import { TasksContext } from "@components/task";
+import { Body, BodySecondary } from "@components/typography";
 import { TaskTitle } from "./task-modal.container.styled";
+import { tagColors } from "@components/tag/tag.component.styled";
 
 interface TaskModalContainerProps {
   onClose(open: false): void;
@@ -38,6 +43,27 @@ export const TaskModalContainer: React.FC<TaskModalContainerProps> = ({ show, on
       >
         {task.title}
       </TaskTitle>
+      <Separator size={Spacing.Small} />
+      {task.description && <BodySecondary>{task.description}</BodySecondary>}
+      <Separator size={Spacing.Small} />
+      <Divider />
+      <Separator size={Spacing.Small} />
+      <Flex>
+        <Flex.Item noGrow vAlign="center">
+          <Body>Status:</Body>
+        </Flex.Item>
+        <Flex.Separator />
+        {(() => {
+          const tagColor = tagColors[task.status];
+          return (
+            <Flex.Item hAlign="flex-start" vAlign="center">
+              <Tag bgColor={tagColor?.bg} textColor={tagColor?.text}>
+                {task.status}
+              </Tag>
+            </Flex.Item>
+          );
+        })()}
+      </Flex>
     </Modal>
   );
 };
